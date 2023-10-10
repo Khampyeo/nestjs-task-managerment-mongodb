2fa689c08ff466a3f9e2941258e4f597bdfea31d
Nguyen Chanh Kiet <107877715+Khampyeo@users.noreply.github.com>
2023-10-10 12:52:49 +0700

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as config from 'config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/task-management'),
+    MongooseModule.forRoot(
+      `${config.get('db').LOCAL_DATABASE || process.env.DATABASE}`,
+    ),
     TasksModule,
     AuthModule,
   ],
